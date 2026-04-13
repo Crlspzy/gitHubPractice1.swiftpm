d035e0ed63a9abd4dbe440fdf1c1fc01375b9a76
EJ Marshall <emarshall9247@stu.d214.org>
2026-04-13 10:42:08 -0500

    i added a textfield

--- ContentView.swift
+++ ContentView.swift
@@ -1,8 +1,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var name = ""
     var body: some View {
      Text("Evan")
         Text("Olivia")
+        TextField("name", text: $name)
+        Text(name)
     }
 }
