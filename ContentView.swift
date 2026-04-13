77ee4d4bb8ab5295de5aef228f5bc4799e6b986a
Olivia <olivia.szeszko@gmail.com>
2026-04-13 11:11:15 -0500

    added a navigation to the new page called PageView

--- ContentView.swift
+++ ContentView.swift
@@ -4,37 +4,42 @@ struct ContentView: View {
     @State var name = ""
     @State var number = 0
     var body: some View {
-        Image("ball")
-            .resizable()
-            .frame(width: 150, height: 150)
-            .border(Color.cyan, width: 5)
-     Text("Evan")
-     
-        Text("\(number)")
-        Text("Evan")
-            .font(.system(size: 50, weight: .light, design: .serif))
-            .foregroundStyle(.blue)
-        Text("Olivia")
-            .font(.custom("American Typewriter", size: 70))
-            .foregroundStyle(.pink)
-        TextField("name", text: $name)
-            .textFieldStyle(.roundedBorder)
-            .font(.title2)
-        Text(name)
-        
-        
-        Button {
-            name = ""
-        } label: {
-            Text("Clear")
-                .foregroundStyle(.blue)
-                .font(.system(size: 30, weight: .bold, design: .monospaced))
+        NavigationStack{
+            Image("ball")
+                .resizable()
+                .frame(width: 150, height: 150)
+                .border(Color.cyan, width: 5)
+            Text("Evan")
             
-        }
-        Image(systemName: "arrow.up")
-            .font(.system(size: 100, weight: .bold, design: .default))
-            .onTapGesture {
-                number += 1
+            Text("\(number)")
+            Text("Evan")
+                .font(.system(size: 50, weight: .light, design: .serif))
+                .foregroundStyle(.blue)
+            Text("Olivia")
+                .font(.custom("American Typewriter", size: 70))
+                .foregroundStyle(.pink)
+            TextField("name", text: $name)
+                .textFieldStyle(.roundedBorder)
+                .font(.title2)
+            Text(name)
+            
+            
+            Button {
+                name = ""
+            } label: {
+                Text("Clear")
+                    .foregroundStyle(.blue)
+                    .font(.system(size: 30, weight: .bold, design: .monospaced))
+                
             }
+            Image(systemName: "arrow.up")
+                .font(.system(size: 100, weight: .bold, design: .default))
+                .onTapGesture {
+                    number += 1
+                }
+            NavigationLink("Go to new page") {
+                PageView()
+            }
+        }
     }
 }
