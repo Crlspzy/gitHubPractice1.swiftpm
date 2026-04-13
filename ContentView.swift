7f7652de2a8690c339f8604e547e934a94690f32
Olivia <olivia.szeszko@gmail.com>
2026-04-13 10:42:11 -0500

    added button

--- ContentView.swift
+++ ContentView.swift
@@ -4,5 +4,13 @@ struct ContentView: View {
     var body: some View {
      Text("Evan")
         Text("Olivia")
+        
+        
+        Button {
+            name = ""
+        } label: {
+            Text("Clear")
+        }
+
     }
 }
