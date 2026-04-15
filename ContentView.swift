3d3c81e511f75a364a575c2e39395edf162ed040
jhhsxcode <jhhs.xcode@d214.org>
2026-04-15 10:03:20 -0500

    Added a brace to get it working

--- ContentView.swift
+++ ContentView.swift
@@ -16,29 +16,15 @@ struct ContentView: View {
                 .font(.title)
             Text("Evan")
                 .font(.system(size: 50, weight: .light, design: .serif))
-        Image("ball")
-            .resizable()
-            .frame(width: 150, height: 150)
-            .border(Color.cyan, width: 5)
-        
-        
-        Text("\(number)")
-        Text("Evan")
-            .font(.system(size: 50, weight: .light, design: .serif))
-            .foregroundStyle(.blue)
-        Text("Olivia")
-            .font(.custom("American Typewriter", size: 70))
-            .foregroundStyle(.pink)
-        TextField("name", text: $name)
-            .textFieldStyle(.roundedBorder)
-            .font(.title2)
-        Text(name)
-        
-        
-        Button {
-            name = ""
-        } label: {
-            Text("Clear")
+            Image("ball")
+                .resizable()
+                .frame(width: 150, height: 150)
+                .border(Color.cyan, width: 5)
+            
+            
+            Text("\(number)")
+            Text("Evan")
+                .font(.system(size: 50, weight: .light, design: .serif))
                 .foregroundStyle(.blue)
             Text("Olivia")
                 .font(.custom("American Typewriter", size: 70))
@@ -54,33 +40,48 @@ struct ContentView: View {
             } label: {
                 Text("Clear")
                     .foregroundStyle(.blue)
-                    .font(.system(size: 30, weight: .bold, design: .monospaced))
+                Text("Olivia")
+                    .font(.custom("American Typewriter", size: 70))
+                    .foregroundStyle(.pink)
+                TextField("name", text: $name)
+                    .textFieldStyle(.roundedBorder)
+                    .font(.title2)
+                Text(name)
                 
+                
+                Button {
+                    name = ""
+                } label: {
+                    Text("Clear")
+                        .foregroundStyle(.blue)
+                        .font(.system(size: 30, weight: .bold, design: .monospaced))
+                    
+                }
+                Image(systemName: "arrow.up")
+                    .font(.system(size: 100, weight: .bold, design: .default))
+                    .onTapGesture {
+                        number += 1
+                    }
+                NavigationLink("Go to new page") {
+                    PageView()
+                }
             }
             Image(systemName: "arrow.up")
                 .font(.system(size: 100, weight: .bold, design: .default))
                 .onTapGesture {
-                    number += 1
+                    number += clicks
                 }
-            NavigationLink("Go to new page") {
-                PageView()
-            }
+            Text("x2")
+                .font(.system(size: 50, weight: .bold, design: .default))
+                .onTapGesture {
+                    
+                    if number < 10 {
+                        clicks = clicks
+                    } else {
+                        clicks *= 2
+                        number -= 10
+                    }
+                }
         }
-        Image(systemName: "arrow.up")
-            .font(.system(size: 100, weight: .bold, design: .default))
-            .onTapGesture {
-                number += clicks
-            }
-        Text("x2")
-            .font(.system(size: 50, weight: .bold, design: .default))
-            .onTapGesture {
-                
-                if number < 10 {
-                    clicks = clicks
-                } else {
-                    clicks *= 2
-                    number -= 10
-                }
-            }
     }
 }
